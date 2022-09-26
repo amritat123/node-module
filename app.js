972c6fad1c325caff1102f453e1f95173b002266
Your Name <you@example.com>
2022-09-26 11:35:03 +0530

    exporting module

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // How to exports and require modules with nodejs
+// If we want tovuse file system with node. we just require
+
+
+// file system are core module
+const file_system = require('file_system');
+
+// If I want to use it module file in app.js file so bring it in.
+const fun1 = require("./module");
+
--- module.js
+++ module.js
@@ -1,1 +1,13 @@
 // modules are indivisual file of node js.
+// Here we have javascript file
+
+const fun1= function (){
+    console.log ("This is function fun1 inside of modules.js file");
+}
+
+const fun2 = function () {
+    console.log(" this is function fun2 inside of module.js file");
+}
+
+// if exporting FUNC2 will be visible to public || but fun2 will be PRIVATE in this file only.
+module.exports = fun1;
